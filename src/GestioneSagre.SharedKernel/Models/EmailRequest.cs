16a6d4fb2a8c670bd79485334e7181296f748e51
Angelo Pirola <angelo9871@gmail.com>
2023-02-21 23:21:29 +0100

    Aggiunti models

---
+++ src/GestioneSagre.SharedKernel/Models/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneSagre.SharedKernel.Models;
+
+public class EmailRequest
+{
+    public Guid MessageId { get; set; }
+    public string RecipientEmail { get; set; }
+    public string Subject { get; set; }
+    public string HtmlMessage { get; set; }
+}
---
+++ src/GestioneSagre.SharedKernel/Models/EmailSendResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace GestioneSagre.SharedKernel.Models;
+
+public class EmailSendResponse
+{
+    public bool Succeeded { get; set; }
+}
