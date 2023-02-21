869bb9e1d14a6c2799a660aae72160f358fa94db
Angelo Pirola <angelo9871@gmail.com>
2023-02-21 23:48:48 +0100

    Fix

---
+++ src/GestioneSagre.SharedKernel/Models/Email/EmailResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace GestioneSagre.SharedKernel.Models.Email;
+
+public class EmailResponse
+{
+    public int Id { get; set; }
+    public Guid MessageId { get; set; }
+    public string Recipient { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public int SenderCount { get; set; }
+    public MailStatus Status { get; set; }
+}
