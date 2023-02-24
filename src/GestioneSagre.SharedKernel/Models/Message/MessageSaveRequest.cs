04b29125b8be1c17ea7297a175e8955cc2d798b9
Angelo Pirola <angelo9871@gmail.com>
2023-02-24 23:16:58 +0100

    Aggiunta nuova request

---
+++ src/GestioneSagre.SharedKernel/Models/Message/MessageSaveRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace GestioneSagre.SharedKernel.Models.Message;
+
+public class MessageSaveRequest
+{
+    public Guid MessageId { get; set; }
+    public string Recipient { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public int SenderCount { get; set; }
+    public MailStatus Status { get; set; }
+}
